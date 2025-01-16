8e693eea34d668997d9b1823635765520724524d
justgotothedesk <wlscjf3877@naver.com>
2025-01-16 13:32:05 +0900

    refactor: 최대 파일 업로드 용량 100MB로 변경

--- src/main/java/com/example/tika/FileUploadExceptionAdvice.java
+++ src/main/java/com/example/tika/FileUploadExceptionAdvice.java
@@ -1,16 +1,23 @@
 package com.example.tika;
 
-import org.springframework.ui.Model;
+import com.example.tika.DTO.FileInfoDTO;
+import com.example.tika.DTO.FileResponseDTO;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @ControllerAdvice
 public class FileUploadExceptionAdvice {
 
     @ExceptionHandler(MaxUploadSizeExceededException.class)
-    public String handleMaxSizeException(MaxUploadSizeExceededException exc, Model model) {
-        model.addAttribute("message", "File size exceeds the maximum limit (50MB). Please upload a smaller file.");
-        return "index";
+    public ResponseEntity<FileResponseDTO> handleMaxSizeException(MaxUploadSizeExceededException exc) {
+        List<FileInfoDTO> fileInfoList = new ArrayList<>();
+        fileInfoList.add(new FileInfoDTO("Fail", "Fail", "File size exceeds the maximum limit (100MB). Please upload a smaller file."));
+
+        return ResponseEntity.ok(new FileResponseDTO(1, fileInfoList));
     }
 }
